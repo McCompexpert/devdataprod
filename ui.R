2db48700dd77d5e4ec5eac19dfc369d1b8ffd1c4
McCompexpert <McCompexpert>
2014-07-27 20:47:15 +0200

    added slidify presentation

--- ui.R
+++ ui.R
@@ -11,6 +11,18 @@ shinyUI(fluidPage(
         titlePanel("Germany's Some Numbers"),
         # Sidebar with controls to select the year to display in Geo Chart
         sidebarLayout(sidebarPanel(
-                selectInput("dataYear", "Select Year:", choices=colnames(phones[,-8])),hr()),
+                selectInput("dataYear", "Select Year:", choices=colnames(phones[,-8])),hr(),
+				br(),
+				p(strong(em("Documentation:"))),
+				br(),
+				p(strong(em("1. Select the year in the drop down"))),
+				br(),
+				p(strong(em("2. GoogleViz chart will display Germany's Map"))),
+				br(),
+				p(strong(em("3. Enjoy the reactive output"))),
+				br(),
+				p(strong(em("Data: is purely simulated, just to demonstrate the reactivity"))),
+				br(),
+				p(strong(em("Github repository:",a("Developing Data Products - devdataprod",href="https://github.com/McCompexpert/devdataprod"))))	),
                 # Show the google map
                 mainPanel(h3(textOutput("dataYear")),htmlOutput("view")))))
